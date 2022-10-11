83b39999cefb79560a6478c7297a084a13ff66f9
seraleman <seraleman1204@gmail.com>
2022-10-11 17:56:15 -0500

    Commit inicial

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+
+const app = express()
+
+const port = 8013
+
+app.set('view engine', 'hbs')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/generic', (req, res) => {
+  res.sendFile(__dirname + '/public/generic.html')
+})
+
+app.get('/elements', (req, res) => {
+  res.sendFile(__dirname + '/public/elements.html')
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/public/404.html')
+})
+
+app.listen(port, () => {
+  console.log(`Escuchando en http://localhost:${port}`)
+})
